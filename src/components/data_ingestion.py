31d078bf3b552e92c6926e645d46383e55dd6700
diljyotsingh019 <diljyotsingh019@gmail.com>
2024-03-11 17:24:51 -0400

    new commit

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,25 +1,55 @@
 import pandas as pd
 import numpy as np
+import sys 
+import os
 from src.logger.logging import logging
 from src.exception.exception import custom_exception
-import os
-import sys 
+
 from sklearn.model_selection import train_test_split 
 from dataclasses import dataclass 
 from pathlib import Path
 
 @dataclass
 class DataIngestionConfig:
-    pass 
+    raw_data_path :str =  os.path.join("artifacts", "raw.csv")
+    train_data_path :str =  os.path.join("artifacts", "train.csv")
+    test_data_path :str =  os.path.join("artifacts", "test.csv")
 
 class DataIngestion:
     def __init__(self):
-        pass 
+        self.ingestion_config = DataIngestionConfig()
 
     def initiate_data_ingestion(self):
+        logging.info("Data Ingestion Initiated")
         try:
-            pass
+            data = pd.read_csv("https://raw.githubusercontent.com/sunnysavita10/fsdsmendtoend/main/notebooks/data/gemstone.csv")
+            logging.info("Reading a data frame")
+            os.makedirs(os.path.dirname(os.path.join(self.ingestion_config.raw_data_path)),exist_ok=True)
+            data.to_csv(self.ingestion_config.raw_data_path,index=False)
+            logging.info("Saving the raw dataset in artifact folder")
+            
+            logging.info("Perrforming train test split")
+            
+            train_data,test_data=train_test_split(data,test_size=0.25)
+            logging.info("train test split completed")
+            
+            train_data.to_csv(self.ingestion_config.train_data_path,index=False)
+            test_data.to_csv(self.ingestion_config.test_data_path,index=False)
+            
+            logging.info("data ingestion completed")
+            
+            return (
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+
+
+
         except Exception as e:
             logging.info()
             raise custom_exception(e, sys)
     
+    if __name__ == "__main__":
+        obj = DataIngestion()
+
+        obj.initiate_data_ingestion()
